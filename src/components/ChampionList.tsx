cbbffce4fd8230766bc5f7ea3659d704fb08058e
dwatring <dwatring4@hotmail.com>
2024-08-05 15:09:20 -0500

    simplify

--- src/components/ChampionList.tsx
+++ src/components/ChampionList.tsx
@@ -3,43 +3,26 @@ import './ChampionList.css'
 import { makeObservable } from 'mobx'
 import { observer } from 'mobx-react'
 
-interface Champion {
-  image: string
-  name: string
-}
-
-const champions: Array<Champion> = [
-  {
-    image: '', // TODO
-    name: 'Thresh',
-  },
-  {
-    image: '', // TODO
-    name: 'Braum',
-  },
-]
-
 export default class ChampionList extends React.Component {
   render() {
     return (
       <div>
-        {champions.map((champion) => (
-          <ChampionListItem key={champion.name} champion={champion} />
-        ))}
+        <ChampionListItem key={'Thresh'} name={'Thresh'} />
+        <ChampionListItem key={'Braum'} name={'Braum'} />
       </div>
     )
   }
 }
 
 interface ChampionListItemProps {
-  champion: Champion
+  name: string
 }
 
 export class ChampionListItem extends React.Component<ChampionListItemProps> {
   render() {
     return (
       <div className="container">
-        <p className="championName">{this.props.champion.name}</p>
+        <p className="championName">{this.props.name}</p>
       </div>
     )
   }
