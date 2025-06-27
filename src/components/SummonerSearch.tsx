ba7bb42a24b2cc8002f9365c11103cf67cea62b1
Frank Watring <fwatring27@gmail.com>
2025-06-27 09:36:58 -0500

    Rename convention

--- src/components/SummonerSearch.tsx
+++ src/components/SummonerSearch.tsx
@@ -1235,7 +1235,7 @@ export default class SummonerSearch extends React.Component {
                                                             </table>
                                                             {/* Objectives Display*/}
                                                             <div className='matchDetailsObjectiveDisplay'>
-                                                                <li className="icon-list-item">
+                                                                <li className="iconListItem">
                                                                     <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                                                         <path
                                                                             fill="currentColor"
