84f615adee9b5c38b3f146805421d0140d96535e
Frank Watring <fwatring27@gmail.com>
2025-06-27 09:36:49 -0500

    Wrap the svg to a list

--- src/components/SummonerSearch.tsx
+++ src/components/SummonerSearch.tsx
@@ -1235,13 +1235,15 @@ export default class SummonerSearch extends React.Component {
                                                             </table>
                                                             {/* Objectives Display*/}
                                                             <div className='matchDetailsObjectiveDisplay'>
-                                                                <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                                                    <path
-                                                                        fill="currentColor"
-                                                                        fillRule="nonzero"
-                                                                        d="M9 10a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7 8a1 1 0 1 1 2 0 1 1 0 0 1-2 0m0 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0m-2-2a1 1 0 1 1 2 0 1 1 0 0 1-2 0m5-10 2 4-1 1H9L8 4 7 5H5L4 4l2-4-6 4 2 4 3 8 1-1h4l1 1 3-8 2-4z"
-                                                                    ></path>
-                                                                </svg>
+                                                                <li className="icon-list-item">
+                                                                    <svg width="20" height="20" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                                                        <path
+                                                                            fill="currentColor"
+                                                                            fillRule="nonzero"
+                                                                            d="M9 10a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7 8a1 1 0 1 1 2 0 1 1 0 0 1-2 0m0 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0m-2-2a1 1 0 1 1 2 0 1 1 0 0 1-2 0m5-10 2 4-1 1H9L8 4 7 5H5L4 4l2-4-6 4 2 4 3 8 1-1h4l1 1 3-8 2-4z"
+                                                                        />
+                                                                    </svg>
+                                                                </li>
                                                             </div>
 
                                                             <table className="championMatchDetailsTableRed">
