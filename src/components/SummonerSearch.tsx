8c240245f7a5da775bdb261b4066d4c153dd1bce
Frank Watring <fwatring27@gmail.com>
2025-06-27 10:28:21 -0500

    create a iconListItem instance of inhibitors slain for blue team

--- src/components/SummonerSearch.tsx
+++ src/components/SummonerSearch.tsx
@@ -1300,6 +1300,19 @@ export default class SummonerSearch extends React.Component {
                                                                     </svg>
                                                                     <span className="iconTurretBlue">x</span>
                                                                 </li>
+                                                                <li className="iconListItem">
+                                                                    <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+                                                                        <path
+                                                                            fill="currentColor"
+                                                                            fillRule="nonzero"
+                                                                            d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14m0-1A6 6 0 1 0 8 2a6 6 0 0 0 0 12" />
+                                                                    </svg>
+                                                                    <path
+                                                                        fill="currentColor"
+                                                                        fillRule="nonzero"
+                                                                        d="m8 4 4 4-4 4-4-4z" />
+                                                                    <span className="iconInhibitorBlue">x</span>
+                                                                </li>
 
                                                             </div>
 
