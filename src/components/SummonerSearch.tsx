68e118661bd540f2f7c16d6b16540e7413491813
Frank Watring <fwatring27@gmail.com>
2025-06-27 09:42:26 -0500

    rename iconLabel to baron association

--- src/components/SummonerSearch.tsx
+++ src/components/SummonerSearch.tsx
@@ -1243,7 +1243,7 @@ export default class SummonerSearch extends React.Component {
                                                                             d="M9 10a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7 8a1 1 0 1 1 2 0 1 1 0 0 1-2 0m0 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0m-2-2a1 1 0 1 1 2 0 1 1 0 0 1-2 0m5-10 2 4-1 1H9L8 4 7 5H5L4 4l2-4-6 4 2 4 3 8 1-1h4l1 1 3-8 2-4z"
                                                                         />
                                                                     </svg>
-                                                                    <span className="iconLabel">x</span>
+                                                                    <span className="iconBaronBlue">x</span>
                                                                 </li>
                                                             </div>
 
