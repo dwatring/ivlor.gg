4a66c04aa85fab56866cf809b591083fd7e53e86
Frank Watring <fwatring27@gmail.com>
2025-06-30 09:15:51 -0500

    change fill and clipping for InhibitorBlue's Icon to display properly

--- src/components/SummonerSearch.tsx
+++ src/components/SummonerSearch.tsx
@@ -1310,14 +1310,18 @@ export default class SummonerSearch extends React.Component {
                                                                         <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
                                                                             <path
                                                                                 fill="currentColor"
+                                                                                fillRule="evenodd"
+                                                                                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14m0-1A6 6 0 1 0 8 2a6 6 0 0 0 0 12"
+                                                                                clipRule="evenodd"
+                                                                            />
+                                                                            <path
+                                                                                fill="currentColor"
                                                                                 fillRule="nonzero"
-                                                                                d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14m0-1A6 6 0 1 0 8 2a6 6 0 0 0 0 12" />
+                                                                                d="m8 4 4 4-4 4-4-4z" />
                                                                         </svg>
-                                                                        <path
-                                                                            fill="currentColor"
-                                                                            fillRule="nonzero"
-                                                                            d="m8 4 4 4-4 4-4-4z" />
+
                                                                         <span className="iconInhibitorBlue">x</span>
+
                                                                     </li>
                                                                 </div>
                                                                 <div className='damageMeterDisplay'>
