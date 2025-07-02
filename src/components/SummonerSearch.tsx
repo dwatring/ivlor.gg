03c7820f8e9e334f0ba0d456ee1861626194e2c5
Frank Watring <fwatring27@gmail.com>
2025-07-02 09:16:29 -0500

    display VoidGrubBlue kills

--- src/components/SummonerSearch.tsx
+++ src/components/SummonerSearch.tsx
@@ -1294,7 +1294,7 @@ export default class SummonerSearch extends React.Component {
                                                                                 clipRule="evenodd"
                                                                             />
                                                                         </svg>
-                                                                        <span className="iconVoidGrubBlue">x</span>
+                                                                        <span className="iconVoidGrubBlue">{match.info.teams[0].objectives.horde.kills}</span>
                                                                     </li>
                                                                     <li className="iconListItemBlue">
                                                                         <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
@@ -1303,7 +1303,7 @@ export default class SummonerSearch extends React.Component {
                                                                                 fillRule="nonzero"
                                                                                 d="m12 8-2 8H6L4 8l4 4zM8 0l4 4-1.003 1.002L11 5h3l-6 6-6-6h2.999L4 4zm0 2.4L6.4 4 8 5.6 9.6 4z" />
                                                                         </svg>
-                                                                        <span className="iconTurretBlue">x</span>
+                                                                        <span className="iconTurretBlue"></span>
                                                                     </li>
                                                                     <li className="iconListItemBlue">
                                                                         <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
