eecd288edfc4f4c115253527945fd433461684ca
Frank Watring <fwatring27@gmail.com>
2025-07-02 09:21:04 -0500

    fix team to start on red

--- src/components/SummonerSearch.tsx
+++ src/components/SummonerSearch.tsx
@@ -1337,7 +1337,7 @@ export default class SummonerSearch extends React.Component {
                                                                                 d="M9 10a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7 8a1 1 0 1 1 2 0 1 1 0 0 1-2 0m0 4a1 1 0 1 1 2 0 1 1 0 0 1-2 0m-2-2a1 1 0 1 1 2 0 1 1 0 0 1-2 0m5-10 2 4-1 1H9L8 4 7 5H5L4 4l2-4-6 4 2 4 3 8 1-1h4l1 1 3-8 2-4z"
                                                                             />
                                                                         </svg>
-                                                                        <span className="iconBaronRed">{match.info.teams[0].objectives.baron.kills}</span>
+                                                                        <span className="iconBaronRed">{match.info.teams[1].objectives.baron.kills}</span>
                                                                     </li>
                                                                     <li className="iconListItemRed">
                                                                         <svg width="16" height="16" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
